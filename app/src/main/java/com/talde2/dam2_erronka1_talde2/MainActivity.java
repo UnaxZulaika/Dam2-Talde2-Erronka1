036c6d75b61a74eedae6a78686a4d2dd1cdb2de3
UnaxZulaika <unax.zulaikafu@elorrieta-errekamari.com>
2023-09-21 14:10:33 +0100

    0.03 (Hasierako pantaila, Login pantailara pasatzen da 1seg ondoren.)

--- app/src/main/java/com/talde2/dam2_erronka1_talde2/MainActivity.java
+++ app/src/main/java/com/talde2/dam2_erronka1_talde2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.talde2.dam2_erronka1_talde2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Sortu intent bat login hasteko
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+                finish(); // Honek MainActivity ixten du, erabiltzaileak Atzera botoia erabiliz bertara itzultzea nahi ez baduzu.
+            }
+        }, 1000); // zenbat denbora tardatzen duen aldatzen 1sg = 1000
     }
 }

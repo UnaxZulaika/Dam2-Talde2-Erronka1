069d06098e2513df2ffea3b165e51160e5902efa
Unax <unax.zulaikafu@elorrieta-errekamari.com>
2023-09-21 11:59:53 +0400

    0.01

---
+++ app/src/main/java/com/talde2/dam2_erronka1_talde2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.talde2.dam2_erronka1_talde2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DAM2-Erronka1-Talde2"
+include(":app")
+ 

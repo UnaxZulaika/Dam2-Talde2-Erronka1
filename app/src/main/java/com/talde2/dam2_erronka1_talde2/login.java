9b4def7afe058a196bfd7b98b28afa85a4b044b9
UnaxZulaika <unax.zulaikafu@elorrieta-errekamari.com>
2023-09-24 22:15:20 +0200

    0.05 (ID guztiak aldatuta, Erregistro pantaila osoa eginda (Bisual)

--- app/src/main/java/com/talde2/dam2_erronka1_talde2/login.java
+++ app/src/main/java/com/talde2/dam2_erronka1_talde2/login.java
@@ -1,8 +1,11 @@
 package com.talde2.dam2_erronka1_talde2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnErregistratu = findViewById(R.id.loginBtnErregistratu);
+        btnErregistratu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, register.class);
+                startActivity(intent);
+            }
+        });
     }
 }

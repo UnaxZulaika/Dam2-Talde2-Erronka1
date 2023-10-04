b9ea4fee044046d6e3992ad3790439f836ba31b0
“Hodei” <hodei.martinezag@elorrieta-errekamari.com>
2023-10-04 13:16:12 +0200

    0.112 (Erreserba Konfirmazioa eta erreserba informazioa layout)

---
+++ app/src/main/java/com/talde2/dam2_erronka1_talde2/ErreserbaTxartela.java
@@ -0,0 +1,14 @@
+package com.talde2.dam2_erronka1_talde2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErreserbaTxartela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erreserba_txartela);
+    }
+}

ec99e973834b174cc45811c3ad296571f660ee17
“Iker” <iker.gorostiagala@elorrieta-errekamari.com>
2023-10-09 12:27:57 +0200

    0.132 (nire kontuan informazioa agertzea(menuko pantailatik informazia pasatzea pantailatik) !erreserbak pantaila menuan bug: menua irekita agertzen da)

--- app/src/main/java/com/talde2/dam2_erronka1_talde2/erreserbak.java
+++ app/src/main/java/com/talde2/dam2_erronka1_talde2/erreserbak.java
@@ -9,20 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.talde2.dam2_erronka1_talde2.Objetuak.Erabiltzaile;
+
 public class erreserbak extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_erreserbak);
+    //MENUA
         //menua izkutatzeko lehenik
         LinearLayout opzioak = findViewById(R.id.idLayOpzioak);
         opzioak.setVisibility(View.INVISIBLE);
 
-    //MENUA
         Button btnMenuItx = findViewById(R.id.idBtnMenuItxi);
         Button btnMenuZabaldu = findViewById(R.id.IdBtnMenuZabaldu);
-        LinearLayout Opzioak = findViewById(R.id.idLayOpzioak);
 
         //open
         btnMenuZabaldu.setOnClickListener(new View.OnClickListener(){
@@ -47,21 +48,36 @@ public class erreserbak extends AppCompatActivity {
         btnAtzera.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                LinearLayout Opzioak = findViewById(R.id.idLayOpzioak);
-                Opzioak.setVisibility(View.INVISIBLE);
+                LinearLayout opzioak = findViewById(R.id.idLayOpzioak);
+                opzioak.setVisibility(View.VISIBLE);
             }
         });
 
 
     //LEHIOAK PASATZERA
-
+        //datos a pasar a todas las pantallas del menu (para nire kontua)
+        String izena = getIntent().getStringExtra("USER_izena");
+        String abizena = getIntent().getStringExtra("USER_abizena");
+        String nan = getIntent().getStringExtra("USER_nan");
+        String email = getIntent().getStringExtra("USER_email");
+        String mugikorra = getIntent().getStringExtra("USER_mugikorra");
+        String erabiltzaileMota = getIntent().getStringExtra("USER_erabiltzaileMota");
         //NIRE KONTUA
         Button btnNireKontua = findViewById(R.id.idBtnMenuKontua);
         btnNireKontua.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(erreserbak.this, Nire_Kontua.class);
-                startActivity(intent1);
+                Intent intent = new Intent(erreserbak.this, Nire_Kontua.class);
+
+                intent.putExtra("USER_izena", izena);
+                intent.putExtra("USER_abizena", abizena);
+                intent.putExtra("USER_nan", nan);
+                intent.putExtra("USER_email", email);
+                intent.putExtra("USER_mugikorra", mugikorra);
+                intent.putExtra("USER_erabiltzaileMota", erabiltzaileMota);
+
+                startActivity(intent);
             }
         });
 
@@ -70,8 +86,16 @@ public class erreserbak extends AppCompatActivity {
         btnErregistratu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(erreserbak.this, Egindako_Erreserbak.class);
-                startActivity(intent2);
+                Intent intent = new Intent(erreserbak.this, Egindako_Erreserbak.class);
+
+                intent.putExtra("USER_izena", izena);
+                intent.putExtra("USER_abizena", abizena);
+                intent.putExtra("USER_nan", nan);
+                intent.putExtra("USER_email", email);
+                intent.putExtra("USER_mugikorra", mugikorra);
+                intent.putExtra("USER_erabiltzaileMota", erabiltzaileMota);
+
+                startActivity(intent);
             }
         });
 
@@ -80,8 +104,16 @@ public class erreserbak extends AppCompatActivity {
         idBtnPribatutasunPolitikak.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(erreserbak.this, PribatasunPolitikak.class);
-                startActivity(intent1);
+                Intent intent = new Intent(erreserbak.this, PribatasunPolitikak.class);
+
+                intent.putExtra("USER_izena", izena);
+                intent.putExtra("USER_abizena", abizena);
+                intent.putExtra("USER_nan", nan);
+                intent.putExtra("USER_email", email);
+                intent.putExtra("USER_mugikorra", mugikorra);
+                intent.putExtra("USER_erabiltzaileMota", erabiltzaileMota);
+
+                startActivity(intent);
             }
         });
 
@@ -106,7 +138,7 @@ public class erreserbak extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+//menua fin
 
     }
 
